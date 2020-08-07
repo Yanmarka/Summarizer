9c3270456832c477fa50381d11fc7c5e4c5c46bc
Yannick Wehr <info@yannick-wehr.de>
2020-08-07 12:18:24 +0200

    Added time measurement

--- rater.py
+++ rater.py
@@ -8,7 +8,7 @@ model = "cnnd16k.model"
 TOKENIZER = SentencePieceProcessor()
 TOKENIZER.load(model)
 
-def compute_rouge(path="testing_results.txt"):
+def compute_rouge(path="model_output.txt"):
     fscore = 0
     precision = 0
     recall = 0
@@ -17,10 +17,14 @@ def compute_rouge(path="testing_results.txt"):
         testing_results = json.load(f)
 
     for i, element in enumerate(testing_results):
-        scores = rouge.get_scores(TOKENIZER.DecodeIds(element['hypothesis']), TOKENIZER.DecodeIds(element['reference']))
-        fscore += scores[0]["rouge-1"]["f"]
-        precision += scores[0]["rouge-1"]["p"]
-        recak += scores[0]["rouge-1"]["r"]
+        try:
+            scores = rouge.get_scores(TOKENIZER.DecodeIds(element['prediction']), TOKENIZER.DecodeIds(element['reference']))
+            fscore += scores[0]["rouge-1"]["f"]
+            precision += scores[0]["rouge-1"]["p"]
+            recall += scores[0]["rouge-1"]["r"]
+        except RecursionError:
+            print("Error")
+
 
     fscore = fscore / i
     precision = precision / i
@@ -29,6 +33,17 @@ def compute_rouge(path="testing_results.txt"):
     print("FScore: " + str(fscore))
     print("Precision: " + str(precision))
     print("Recall: "+ str(recall))
+
+def compute_time(path):
+    total = 0
+    k = 0
+    with open(path, 'r') as f:
+        for i, line in enumerate(f.readlines()):
+            if i % 14 == 0:
+                total += float(line.split()[-2])
+                k += 1
+    return total / k
+
 
 if __name__ == "__main__":
     compute_rouge()
