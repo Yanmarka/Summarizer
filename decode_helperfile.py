5710d2ca93b71ac2426db76aaa09e87b6ef00c8f
Yannick Wehr <info@yannick-wehr.de>
2020-08-12 11:30:37 +0200

    Update to decode helperfile

--- decode_helperfile.py
+++ decode_helperfile.py
@@ -1,27 +1,22 @@
 import json
-import dropbox
 import greedy_decoder
 import summarizer
 import data_handler
 
 
 loader = data_handler.Loader()
-loader.load_file("cnn_dailymail_v11_test.txt")
-dbx = dropbox.Dropbox('YOUR_ACCESS_TOKEN')
+loader.load_file("cnn_dailymail_v11_validation.txt")
 
 model = summarizer.my_transformer_lm(mode='eval')
 model.init_from_file("modelc1t150k.pkl")
 
 result_list = []
-for i, element in enumerate(loader.data['test']):
+for i, element in enumerate(loader.data['validation']):
   prediction = greedy_decoder.greedy_predict(element['article'], model)
   result_list.append({'article': element['article'], 'prediction': prediction, 'reference': element['highlights']})
 
   if i % 10 == 0:
-      f = open("model_output_c1t150k", "w")
+      f = open("model_output_c1t150k.txt", "w")
       f.write(json.dumps(result_list))
       f.close()
-  if i % 200 == 0:
-      name = "model_output_c1t150k" + str(i)
-      path = name + ".txt"
-      dbx.files_upload(name, path)
+      print(i)
