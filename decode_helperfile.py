de2820301c962975c80d993cc9e2d27ef6f4a8cd
Yannick Wehr <info@yannick-wehr.de>
2020-08-15 11:19:33 +0200

    Decode adjustments

--- decode_helperfile.py
+++ decode_helperfile.py
@@ -3,20 +3,24 @@ import greedy_decoder
 import summarizer
 import data_handler
 
+def main(model_name, stop=5000):
+    loader = data_handler.Loader()
+    loader.load_file("cnn_dailymail_v11_validation.txt")
 
-loader = data_handler.Loader()
-loader.load_file("cnn_dailymail_v11_validation.txt")
+    model = summarizer.my_transformer_lm(mode='eval')
+    model.init_from_file(model_name)
 
-model = summarizer.my_transformer_lm(mode='eval')
-model.init_from_file("modelc1t150k.pkl")
+    path = model_name + "_output.txt"
+    result_list = []
+    for i, element in enumerate(loader.data['validation']):
+        prediction = greedy_decoder.greedy_predict(element['article'], model)
+        result_list.append({'article': element['article'], 'prediction': prediction, 'reference': element['highlights']})
 
-result_list = []
-for i, element in enumerate(loader.data['validation']):
-  prediction = greedy_decoder.greedy_predict(element['article'], model)
-  result_list.append({'article': element['article'], 'prediction': prediction, 'reference': element['highlights']})
+        if i % 10 == 0:
+            f = open(path, "w")
+            f.write(json.dumps(result_list))
+            f.close()
+            print(i)
 
-  if i % 10 == 0:
-      f = open("model_output_c1t150k.txt", "w")
-      f.write(json.dumps(result_list))
-      f.close()
-      print(i)
+        if i == stop:
+            exit()
