c8d21f66cca9edae50444ca4e0d863e8b439aab2
Yannick Wehr <info@yannick-wehr.de>
2020-08-22 20:35:16 +0200

    Adjusted Decoding Module

--- decode_helperfile.py
+++ decode_helperfile.py
@@ -1,18 +1,19 @@
 import json
 import greedy_decoder
-import summarizer
 import data_handler
 
-def main(model_name, stop=5000):
+def main(model_name, model, start=-1, stop=None, dataset='cnn_dailymail_v11_validation.txt'):
     loader = data_handler.Loader()
-    loader.load_file("cnn_dailymail_v11_validation.txt")
+    loader.load_file(dataset)
 
-    model = summarizer.my_transformer_lm(mode='eval')
     model.init_from_file(model_name)
 
     path = model_name + "_output.txt"
     result_list = []
     for i, element in enumerate(loader.data['validation']):
+        if i < start:
+            continue
+
         prediction = greedy_decoder.greedy_predict(element['article'], model)
         result_list.append({'article': element['article'], 'prediction': prediction, 'reference': element['highlights']})
 
