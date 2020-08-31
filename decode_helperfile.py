22626426def62c420e8f91435ebe278423ad1f22
Yannick Wehr <info@yannick-wehr.de>
2020-08-31 10:57:02 +0200

    Adjusted prints in decode helperfile

--- decode_helperfile.py
+++ decode_helperfile.py
@@ -17,11 +17,11 @@ def main(model_name, model, start=-1, stop=None, dataset='cnn_dailymail_v11_vali
         prediction = greedy_decoder.greedy_predict(element['article'], model)
         result_list.append({'article': element['article'], 'prediction': prediction, 'reference': element['highlights']})
 
-        if i % 10 == 0:
+        if i % 100 == 0:
             f = open(path, "w")
             f.write(json.dumps(result_list))
             f.close()
-            print(i)
+            print("Ran train step " + str(i) + " for model " + model_name)
 
         if i == stop:
             break
