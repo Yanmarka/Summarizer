5bcde6a3f64d794b21f7f6997241cd517abb60b9
Yannick Wehr <info@yannick-wehr.de>
2020-09-14 10:33:20 +0200

    Move trainer to main part of file, use eval data

--- summarizer.py
+++ summarizer.py
@@ -36,15 +36,28 @@ def lm_input_function(n_devices):
     mask = np.array(mask)
     yield (values, values, mask)
 
-def create_trainer(model, inputs, output_dir):
-  trainer = trax.supervised.Trainer(
-      model=model,
-      loss_fn=trax.layers.CrossEntropyLoss(),
-      optimizer=trax.optimizers.Adafactor,
-      lr_schedule=trax.lr.MultifactorSchedule,
-      inputs=inputs,
-      output_dir=output_dir)
-  return trainer
+def lm_input_function_eval(n_devices):
+  batch_size = 8
+  while True:
+    values = []
+    mask = []
+
+    for i in range(n_devices*batch_size):
+      article, summary = loader.load_next_question(ds='validation')
+      article = article + [1]
+      summary = summary + [1]
+      combination = article + [0] + summary
+      padded_text, padding, is_padded = create_padding(combination)
+      values.append(padded_text)
+      if is_padded == True:
+        mask.append(np.concatenate((np.zeros_like(article), [0], np.ones_like(summary), np.zeros_like(padding))))
+      else:
+        mask.append(np.concatenate((np.zeros_like(article), [0], np.ones_like(summary))))
+
+    values = np.array(values)
+    mask = np.array(mask)
+    yield (values, values, mask)
+
 
 if __name__ == "__main__":
     FILE_PATH = "cnn_dailymail_v11.txt"
@@ -63,7 +76,15 @@ if __name__ == "__main__":
     loader.load_file(FILE_PATH)
 
     model = configurations.lsh_reformer(mode='train')
-    inputs = trax.supervised.inputs.Inputs(lm_input_function)
-    trainer = create_trainer(model, inputs, OUTPUT_DIR)
+    summarize_inputs = trax.supervised.inputs.Inputs(train_stream=lm_input_function, eval_stream=lm_input_function_eval)
+
+    trainer = trax.supervised.Trainer(
+      model=model,
+      loss_fn=trax.layers.CrossEntropyLoss(),
+      optimizer=trax.optimizers.Adafactor,
+      lr_schedule=trax.lr.MultifactorSchedule,
+      inputs=summarize_inputs,
+      output_dir=OUTPUT_DIR)
+
     for i in range(EPOCHS):
       trainer.train_epoch(200,10)
