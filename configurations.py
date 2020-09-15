777e266a2a8ea459274fe542bbc4461ffdee8838
Yannick Wehr <info@yannick-wehr.de>
2020-09-15 15:08:31 +0200

    Setting Max length with constant

--- configurations.py
+++ configurations.py
@@ -1,28 +1,30 @@
 import trax
 
+MAX_LEN = 4096
+
 def c1_transformer(mode):
-  return trax.models.TransformerLM(vocab_size=16000, max_len=2048, d_model=1024, d_ff=4096, n_layers=3, mode=mode)
+  return trax.models.TransformerLM(vocab_size=16000, max_len=MAX_LEN, d_model=1024, d_ff=4096, n_layers=3, mode=mode)
 
 def c1_reformer(mode):
-  return trax.models.ReformerLM(vocab_size=16000, max_len=2048, d_model=1024, d_ff=4096, n_layers=3, mode=mode, ff_activation=trax.layers.Relu)
+  return trax.models.ReformerLM(vocab_size=16000, max_len=MAX_LEN, d_model=1024, d_ff=4096, n_layers=3, mode=mode, ff_activation=trax.layers.Relu)
 
 def c2_transformer(mode):
-  return trax.models.TransformerLM(vocab_size=16000, max_len=2048, d_model=1024, d_ff=4096, n_layers=6, mode=mode)
+  return trax.models.TransformerLM(vocab_size=16000, max_len=MAX_LEN, d_model=1024, d_ff=4096, n_layers=6, mode=mode)
 
 def c2_reformer(mode):
-  return trax.models.ReformerLM(vocab_size=16000, max_len=2048, d_model=1024, d_ff=4096, n_layers=6, mode=mode, ff_activation=trax.layers.Relu)
+  return trax.models.ReformerLM(vocab_size=16000, max_len=MAX_LEN, d_model=1024, d_ff=4096, n_layers=6, mode=mode, ff_activation=trax.layers.Relu)
 
 def c3_transformer(mode):
-  return trax.models.TransformerLM(vocab_size=16000, max_len=2048, d_model=1024, d_ff=2048, n_layers=3, mode=mode)
+  return trax.models.TransformerLM(vocab_size=16000, max_len=MAX_LEN, d_model=1024, d_ff=2048, n_layers=3, mode=mode)
 
 def c3_reformer(mode):
-  return trax.models.ReformerLM(vocab_size=16000, max_len=2048, d_model=1024, d_ff=2048, n_layers=3, mode=mode, ff_activation=trax.layers.Relu)
+  return trax.models.ReformerLM(vocab_size=16000, max_len=MAX_LEN, d_model=1024, d_ff=2048, n_layers=3, mode=mode, ff_activation=trax.layers.Relu)
 
 def c4_transformer(mode):
-  return trax.models.TransformerLM(vocab_size=16000, max_len=2048, d_model=1024, d_ff=2048, n_layers=6, mode=mode)
+  return trax.models.TransformerLM(vocab_size=16000, max_len=MAX_LEN, d_model=1024, d_ff=2048, n_layers=6, mode=mode)
 
 def c4_reformer(mode):
-  return trax.models.ReformerLM(vocab_size=16000, max_len=2048, d_model=1024, d_ff=2048, n_layers=6, mode=mode, ff_activation=trax.layers.Relu)
+  return trax.models.ReformerLM(vocab_size=16000, max_len=MAX_LEN, d_model=1024, d_ff=2048, n_layers=6, mode=mode, ff_activation=trax.layers.Relu)
 
 def lsh_reformer(mode):
-  return trax.models.ReformerLM(vocab_size=16000, max_len=2048, d_model=1024, d_ff=4096, n_layers=3, mode=mode, ff_activation=trax.layers.Relu, attention_type=trax.layers.LSHSelfAttention)
+  return trax.models.ReformerLM(vocab_size=16000, max_len=MAX_LEN, d_model=1024, d_ff=4096, n_layers=3, mode=mode, ff_activation=trax.layers.Relu, attention_type=trax.layers.LSHSelfAttention)
